ca25582d979e20884e19b366f6c1143ae4b7df04
DavidAdemola <104461672+DavidAdemola@users.noreply.github.com>
2025-11-12 17:16:17 -0600

    clean up

--- app/src/main/kotlin/com/example/apputbid/MainActivity.kt
+++ app/src/main/kotlin/com/example/apputbid/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.apputbid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -21,7 +20,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
